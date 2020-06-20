eb7f1ac27da80eca2d74b4ced57979aebabbb070
gsolvit <752622384@qq.com>
2020-06-20 10:15:03 +0800

    update

--- main.py
+++ main.py
@@ -1,103 +1,110 @@
 import os
-import time
-import datetime
-import numpy as np
-
-import torch
-import torch.nn.functional as F
-from torchvision.utils import save_image
-
-from model import Generator, Discriminator
+import argparse
+from stargan import StarGAN
+from dataset import get_dataloader
+from torch.backends import cudnn
 
 
-class StarGAN(object):
-    """
-    StarGAN
-    """
-    def __init__(self, celeba_loader, rafd_loader, config):
+def str2bool(v):
+    return v.lower() in ('true')
 
-        # data loader.
-        self.celeba_loader = celeba_loader
-        self.rafd_loader = rafd_loader
 
-        # model configurations.
-        self.c_dim = config.c_dim
-        self.c2_dim = config.c2_dim
-        self.image_size = config.image_size
-        self.g_conv_dim = config.g_conv_dim
-        self.d_conv_dim = config.d_conv_dim
-        self.g_repeat_num = config.g_repeat_num
-        self.d_repeat_num = config.d_repeat_num
-        self.lambda_cls = config.lambda_cls
-        self.lambda_rec = config.lambda_rec
-        self.lambda_gp = config.lambda_gp
+def main(config):
+    # For fast training.
+    cudnn.benchmark = True
 
-        # training configurations.
-        self.dataset = config.dataset
-        self.batch_size = config.batch_size
-        self.num_iters = config.num_iters
-        self.num_iters_decay = config.num_iters_decay
-        self.g_lr = config.g_lr
-        self.d_lr = config.d_lr
-        self.n_critic = config.n_critic
-        self.beta1 = config.beta1
-        self.beta2 = config.beta2
-        self.resume_iters = config.resume_iters
-        self.selected_attrs = config.selected_attrs
+    # Create directories if not exist.
+    if not os.path.exists(config.log_dir):
+        os.makedirs(config.log_dir)
+    if not os.path.exists(config.model_save_dir):
+        os.makedirs(config.model_save_dir)
+    if not os.path.exists(config.sample_dir):
+        os.makedirs(config.sample_dir)
+    if not os.path.exists(config.result_dir):
+        os.makedirs(config.result_dir)
 
-        # test configurations.
-        self.test_iters = config.test_iters
+    # Data loader.
+    celeba_loader = None
+    rafd_loader = None
 
-        # miscellaneous.
-        self.use_tensorboard = config.use_tensorboard
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if config.dataset in ['CelebA', 'Both']:
+        celeba_loader = get_dataloader(config.celeba_image_dir, config.attr_path, config.selected_attrs,
+                                   config.celeba_crop_size, config.image_size, config.batch_size,
+                                   'CelebA', config.mode, config.num_workers)
+    if config.dataset in ['RaFD', 'Both']:
+        rafd_loader = get_dataloader(config.rafd_image_dir, None, None,
+                                 config.rafd_crop_size, config.image_size, config.batch_size,
+                                 'RaFD', config.mode, config.num_workers)
 
-        # directories.
-        self.log_dir = config.log_dir
-        self.sample_dir = config.sample_dir
-        self.model_save_dir = config.model_save_dir
-        self.result_dir = config.result_dir
+    # Solver for training and testing StarGAN.
+    stargan = StarGAN(celeba_loader, rafd_loader, config)
 
-        # step size.
-        self.log_step = config.log_step
-        self.sample_step = config.sample_step
-        self.model_save_step = config.model_save_step
-        self.lr_update_step = config.lr_update_step
+    if config.mode == 'train':
+        if config.dataset in ['CelebA', 'RaFD']:
+            stargan.train()
+        elif config.dataset in ['Both']:
+            stargan.train_multi()
+    elif config.mode == 'test':
+        if config.dataset in ['CelebA', 'RaFD']:
+            stargan.test()
+        elif config.dataset in ['Both']:
+            stargan.test_multi()
 
-        # build the model and tensorboard.
-        self.build_model()
-        if self.use_tensorboard:
-            self.build_tensorboard()
 
-    def build_model(self):
-        """create a generator and a discriminator"""
-        if self.dataset in ['CelebA', 'RaFD']:
-            self.G = Generator(self.g_conv_dim, self.c_dim, self.g_repeat_num)
-            self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim, self.d_repeat_num)
-        elif self.dataset in ['Both']:
-            self.G = Generator(self.g_conv_dim, self.c_dim + self.c2_dim + 2, self.g_repeat_num)  # 2 for mask vector.
-            self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim + self.c2_dim, self.d_repeat_num)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-        self.g_optimizer = torch.optim.Adam(self.G.parameters(), lr=self.g_lr, betas=(self.beta1, self.beta2))
-        self.d_optimizer = torch.optim.Adam(self.D.parameters(), lr=self.d_lr, betas=(self.beta1, self.beta2))
+    # Model configuration.
+    parser.add_argument('--c_dim', type=int, default=5, help='dimension of domain labels (1st dataset)')
+    parser.add_argument('--c2_dim', type=int, default=8, help='dimension of domain labels (2nd dataset)')
+    parser.add_argument('--celeba_crop_size', type=int, default=178, help='crop size for the CelebA dataset')
+    parser.add_argument('--rafd_crop_size', type=int, default=256, help='crop size for the RaFD dataset')
+    parser.add_argument('--image_size', type=int, default=128, help='image resolution')
+    parser.add_argument('--g_conv_dim', type=int, default=64, help='number of conv filters in the first layer of G')
+    parser.add_argument('--d_conv_dim', type=int, default=64, help='number of conv filters in the first layer of D')
+    parser.add_argument('--g_repeat_num', type=int, default=6, help='number of residual blocks in G')
+    parser.add_argument('--d_repeat_num', type=int, default=6, help='number of strided conv layers in D')
+    parser.add_argument('--lambda_cls', type=float, default=1, help='weight for domain classification loss')
+    parser.add_argument('--lambda_rec', type=float, default=10, help='weight for reconstruction loss')
+    parser.add_argument('--lambda_gp', type=float, default=10, help='weight for gradient penalty')
 
-        self.G.to(self.device)
-        self.D.to(self.device)
+    # Training configuration.
+    parser.add_argument('--dataset', type=str, default='CelebA', choices=['CelebA', 'RaFD', 'Both'])
+    parser.add_argument('--batch_size', type=int, default=16, help='mini-batch size')
+    parser.add_argument('--num_iters', type=int, default=200000, help='number of total iterations for training D')
+    parser.add_argument('--num_iters_decay', type=int, default=100000, help='number of iterations for decaying lr')
+    parser.add_argument('--g_lr', type=float, default=0.0001, help='learning rate for G')
+    parser.add_argument('--d_lr', type=float, default=0.0001, help='learning rate for D')
+    parser.add_argument('--n_critic', type=int, default=5, help='number of D updates per each G update')
+    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
+    parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
+    parser.add_argument('--resume_iters', type=int, default=None, help='resume training from this step')
+    parser.add_argument('--selected_attrs', '--list', nargs='+', help='selected attributes for the CelebA dataset',
+                        default=['Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Male', 'Young'])
 
-        self.print_network(self.G, 'G')
-        self.print_network(self.D, 'D')
+    # Test configuration.
+    parser.add_argument('--test_iters', type=int, default=200000, help='test model from this step')
 
-    def print_network(self, model, name):
-        """print out the network information."""
-        num_params = 0
-        for p in model.parameters():
-            num_params += p.numel()
+    # Miscellaneous.
+    parser.add_argument('--num_workers', type=int, default=1)
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
+    parser.add_argument('--use_tensorboard', type=str2bool, default=True)
 
-        print(model)
-        print(name)
-        print('The number of parameters: ', num_params)
+    # Directories.
+    parser.add_argument('--celeba_image_dir', type=str, default='data/celeba/images')
+    parser.add_argument('--attr_path', type=str, default='data/celeba/list_attr_celeba.txt')
+    parser.add_argument('--rafd_image_dir', type=str, default='data/RaFD/train')
+    parser.add_argument('--log_dir', type=str, default='stargan/logs')
+    parser.add_argument('--model_save_dir', type=str, default='stargan/models')
+    parser.add_argument('--sample_dir', type=str, default='stargan/samples')
+    parser.add_argument('--result_dir', type=str, default='stargan/results')
 
-    def build_tensorboard(self):
-        """build a tensorboard logger."""
-        from logger import Logger
-        self.logger = Logger(self.log_dir)
+    # Step size.
+    parser.add_argument('--log_step', type=int, default=10)
+    parser.add_argument('--sample_step', type=int, default=1000)
+    parser.add_argument('--model_save_step', type=int, default=10000)
+    parser.add_argument('--lr_update_step', type=int, default=1000)
+
+    config = parser.parse_args()
+    print(config)
+    main(config)
